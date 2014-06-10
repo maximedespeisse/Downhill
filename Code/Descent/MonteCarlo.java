ea0145f283bac234e0f269e3e26a6e954b033834
maximedespeisse <maxdespeisse@live.fr>
2014-06-10 19:01:23 +0800

    GUI added + MonteCarlo class update

--- Code/Descent/MonteCarlo.java
+++ Code/Descent/MonteCarlo.java
@@ -5,29 +5,29 @@ import Structure.*;
 
 public class MonteCarlo {
 	
-	int iterations;
+	//int iterations;
 	Graph mountain;
-	int bestTotalDistance;
-	ArrayList<Edge> bestPath;
+	public int bestTotalDistance;
+	public ArrayList<Edge> bestPath;
 	Descent descent;
 	
 	/* We run the Monte Carlo experiment */
-	public void run() {	
+	public void run(int iterations) {	
+		System.out.println(iterations);
 		for (int i = 0; i < iterations; i++) {
 			/* We do a new descent */
 			descent = new Descent(mountain);
-			descent.Descend();
+			//descent.Descend();
 			/* If the new solution is better we change the best path and distance */
-			if (bestTotalDistance > descent.totalDistance)
-			{
-				bestTotalDistance = descent.totalDistance;
-				bestPath = descent.path;
-			}
+			//if (bestTotalDistance > descent.totalDistance)
+			//{
+			//	bestTotalDistance = descent.totalDistance;
+			//	bestPath = descent.path;
+			//}
 		}
 	}
 	
-	public MonteCarlo(int iterations_) {
-		iterations = iterations_;
+	public MonteCarlo() {
 		mountain = new Graph();
 		bestTotalDistance = 99999;
 	}
